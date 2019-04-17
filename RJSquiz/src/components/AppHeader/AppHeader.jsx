ffe3768ec484dc5bbd16aa1163a9e06b4c4f6990
David Mieloch <davidmieloch@Davids-MacBook-Pro.local>
2019-04-17 17:59:19 -0400

    add alt tag

--- RJSquiz/src/components/AppHeader/AppHeader.jsx
+++ RJSquiz/src/components/AppHeader/AppHeader.jsx
@@ -4,10 +4,10 @@ const AppHeader = () => {
   return (
     <div className="app-header">
       <div className="app-container">
-        <img className="logo" src='/logo.png'/>
+        <img className="logo" src='/logo.png' alt="logo"/>
         <div className="titles">
           <div className="title">QUIZINATOR</div>
-          <div className="lead">Are you a dummy? {"Let's see..."}</div>
+          <div className="lead">Are you a dummy? Let's see...</div>
         </div>
       </div>
     </div>
